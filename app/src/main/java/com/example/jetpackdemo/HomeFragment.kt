a379eeffdc7e229c10523ada6ced3a19f6302fb4
SunYiH <498611343@qq.com>
2022-02-14 10:21:09 +0800

    Nav初探02

--- app/src/main/java/com/example/jetpackdemo/HomeFragment.kt
+++ app/src/main/java/com/example/jetpackdemo/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.View
 import androidx.databinding.Observable
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
 import com.example.jetpackdemo.base.BaseVMFragment
@@ -79,10 +80,13 @@ class HomeFragment : BaseVMFragment<FragmentMainBinding>(FragmentMainBinding::in
             val bundle = Bundle().apply {
                 putSerializable("repo", it)
             }
+            //一种nav跳转
             findNavController().navigate(R.id.git_fragment, bundle, options)
+
+            //另一种是nav导航图中建立好对应关系,注意：该方法只能再()的click方法中执行,再lambda中无法正常调用
+//            Navigation.createNavigateOnClickListener(R.id.action_home_dest_to_git_fragment, bundle)
+
         }
-
-
 //        lifecycleScope.launch {
 //            gitReposViewModel.apply {
 //                fetchGithubRepos3("sunheihei").observe(this@MainActivity) { result ->
